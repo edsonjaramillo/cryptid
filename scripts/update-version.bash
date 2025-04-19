41d0346a025ccc93e78f44695c8a6086d2f37809
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-04-19 11:14:49 -0500

    feat(docker): add multi-stage Dockerfile for CLI and image release

--- scripts/update-version.bash
+++ scripts/update-version.bash
@@ -17,19 +17,15 @@ fi
 IS_GSED_INSTALLED=false
 IS_SED_INSTALLED=false
 # make sure at least one of gsed or sed is installed
-if ! command -v gsed &> /dev/null; then
-    IS_GSED_INSTALLED=false
+# check that gsed is installed
+if command -v gsed &> /dev/null; then
+    IS_GSED_INSTALLED=true
 fi
 
+# check that sed is installed
 if ! command -v sed &> /dev/null; then
     IS_SED_INSTALLED=false
 fi
-
-if ! $IS_GSED_INSTALLED && ! $IS_SED_INSTALLED; then
-    echo "Error: gsed or sed is not installed"
-    exit 1
-fi
-
 
 # Validate version format 
 if ! [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
@@ -55,8 +51,15 @@ FILES_TO_UPDATE=(
 for FILE in "${FILES_TO_UPDATE[@]}"; do
     if $IS_GSED_INSTALLED; then 
         gsed -i "s/$OLD_VERSION/$NEW_VERSION/g" "$FILE"
-    else
+    fi
+
+    if $IS_SED_INSTALLED; then
         sed -i "s/$OLD_VERSION/$NEW_VERSION/g" "$FILE"
+    fi  
+
+    if ! $IS_GSED_INSTALLED && ! $IS_SED_INSTALLED; then
+        echo "Error: gsed or sed is not installed"
+        exit 1
     fi
 done
 
